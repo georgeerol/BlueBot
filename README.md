5de72f67d81ab8326256378acc1c47a0780e226b
Clifton Craig <clifton.craig@ge.com>
2017-04-08 15:19:32 -0700

    Renames the project from RcCar to AndroidBlueBot

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+**Android BlueBot**
+
+An Android app that controls an Arduino based bot.
 # ![RC Car Logo](https://github.com/fouliex/AndroidRcCar/blob/master/resources/images/RobotCarLogo.jpg) Android RC Car(In Progress)
 ## Wiki Contents
 1. [**Assemble the RC Car**](https://github.com/fouliex/AndroidRcCar/wiki/Assemble-the-RC-Car)
--- app/src/androidTest/java/com/bluebot/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/bluebot/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.car.rc.example.fouliex.rccar;
+package com.bluebot;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.car.rc.example.fouliex.rccar", appContext.getPackageName());
+        assertEquals("com.bluebot", appContext.getPackageName());
     }
 }
