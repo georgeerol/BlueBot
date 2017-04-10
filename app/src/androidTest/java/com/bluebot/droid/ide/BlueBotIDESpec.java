9929e1dff86998104400c69505e42784aa4e1af0
Clifton Craig <clifton.craig@ge.com>
2017-04-09 18:50:47 -0700

    Adds rough/ugly UI via TDD.

--- app/src/androidTest/java/com/bluebot/droid/ide/BlueBotIDESpec.java
+++ app/src/androidTest/java/com/bluebot/droid/ide/BlueBotIDESpec.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
@@ -21,10 +22,20 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 @LargeTest
 public class BlueBotIDESpec {
     @Rule
-    public ActivityTestRule<BlueBotIDEActivity> mActivityRule = new ActivityTestRule(BlueBotIDEActivity.class);
+    public ActivityTestRule<BlueBotIDEActivity> activityTestRule = new ActivityTestRule(BlueBotIDEActivity.class);
 
     @Test
     public void shouldIncludeWindowTitleOnScreen() {
         onView(withText("BlueBot IDE")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldHaveAPlaceToEnterCode() throws Exception {
+        onView(withContentDescription("Code Entry")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldHaveAButtonToRunCode() throws Exception {
+        onView(withContentDescription("Run")).check(matches(isDisplayed()));
     }
 }
