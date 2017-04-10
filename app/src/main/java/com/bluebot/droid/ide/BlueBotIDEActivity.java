3d9cb18ec1386136595c8098410e46508059ff2c
Clifton Craig <clifton.craig@ge.com>
2017-04-09 22:10:41 -0700

    Adds basic response handling to the Activity.

--- app/src/main/java/com/bluebot/droid/ide/BlueBotIDEActivity.java
+++ app/src/main/java/com/bluebot/droid/ide/BlueBotIDEActivity.java
@@ -4,32 +4,48 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 import com.bluebot.R;
 import com.bluebot.droid.ide.runtime.RequestProcessor;
 
+import java.util.Map;
 
-public class BlueBotIDEActivity extends AppCompatActivity {
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+
+public class BlueBotIDEActivity extends AppCompatActivity implements ResponseHandler {
 
     private RequestProcessor requestProcessor;
+    private View errorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blue_bot_ide);
-        ((Button) findViewById(R.id.runButton)).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.runButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final Editable code = ((EditText) findViewById(R.id.codeEntry)).getText();
                 requestProcessor.processRequest(UIRequestTypes.EXECUTE_CODE, code.toString());
             }
         });
+        errorMessage = findViewById(R.id.errorMessage);
+        errorMessage.setVisibility(INVISIBLE);
     }
 
     public void setRequestProcessor(RequestProcessor requestProcessor) {
         this.requestProcessor = requestProcessor;
-        requestProcessor.bind(new ResponseHandler() { });
+        requestProcessor.bind(this);
+    }
+
+    @Override
+    public void responseForRequest(String requestType, Map<String, String> response) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                errorMessage.setVisibility(VISIBLE);
+            }
+        });
     }
 }
--- app/src/main/java/com/bluebot/droid/ide/ResponseHandler.java
+++ app/src/main/java/com/bluebot/droid/ide/ResponseHandler.java
@@ -1,9 +1,12 @@
 package com.bluebot.droid.ide;
 
+import java.util.Map;
+
 /**
  * Created by Clifton Craig on 4/9/17.
  * Copyright GE 4/9/17
  */
 
 public interface ResponseHandler {
+    void responseForRequest(String requestType, Map<String, String> response);
 }
