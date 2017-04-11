72e3047d1db3a0f97b51c6ac81cc657f8b50dd60
Clifton Craig <clifton.craig@ge.com>
2017-04-11 15:21:09 -0700

    Relocates RequestProcessor and ResponseHandler into the Java library.

---
+++ bluebotruntimelib/src/test/java/com/bluebot/ui/RequestProcessorSpec.java
@@ -0,0 +1,16 @@
+package com.bluebot.ui;
+
+import org.junit.Test;
+
+/**
+ * Created by Clifton Craig on 4/11/17.
+ * Copyright GE 4/11/17
+ */
+
+public class RequestProcessorSpec {
+    @Test
+    public void shouldPassRequestsToRelevantCode() throws Exception {
+
+
+    }
+}
